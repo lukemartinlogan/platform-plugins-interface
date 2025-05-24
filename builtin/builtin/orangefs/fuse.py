13111820c63625a97be5b11277849bfe938f83d4
Luke Logan <lukemartinlogan@gmail.com>
2025-05-24 15:41:37 -0500

    Use md list

--- builtin/builtin/orangefs/fuse.py
+++ builtin/builtin/orangefs/fuse.py
@@ -19,8 +19,9 @@ class OrangefsFuse:
 
         # start pfs client
         # pvfs2_fuse = os.path.join(self.orangefs_root, "bin", "pvfs2fuse")
+        md_list = self.md_hosts.list()
         for i,client in self.client_hosts.enumerate():
-            mdm_ip = self.md_hosts.hostname_list()[i % len(self.md_hosts)]
+            mdm_ip = md_list[i % len(self.md_hosts)].hosts_ip[0]
             start_client_cmds = [
                 "pvfs2fuse -o fs_spec={protocol}://{ip}:{port}/pfs {mount_point}".format(
                     pvfs2_fuse=pvfs2_fuse,
